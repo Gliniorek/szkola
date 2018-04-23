b844171b77de13de1c51a2512be5d9a2e84e9068
Anorax0 <jakuhebel@gmail.com>
2018-04-23 22:10:04 +0200

    First commit

--- zadanie_dodatkowe2.py
+++ zadanie_dodatkowe2.py
@@ -5,7 +5,7 @@ if spam.count('a') > spam.count('e'):
     print("W tekście <<", ham, ">> znajduje się więcej liter 'a' niż liter 'e'.")
 elif spam.count('e') > spam.count('a'):
     print("W tekście <<", ham, ">> znajduje się więcej liter 'e' niż liter 'a'.")
-elif spam.count('e') == spam.count('a'):
+elif spam.count('e') == spam.count('a') and (spam.count('a') or spam.count('e')) > 0:
     print("W tekście <<", ham, ">> znajduje się tyle samo liter 'e' jak liter 'a'.")
 else:
     print("W tekście <<", ham, ">> nie ma żadnej litery 'a' lub 'b")
