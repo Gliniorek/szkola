10ac27de4f63c7550e7d6135ac7da3333d30690a
Anorax0 <jakuhebel@gmail.com>
2018-04-23 21:59:19 +0200

    First commit

---
+++ zadanie_dodatkowe2.py
@@ -0,0 +1,11 @@
+ham = input("Wpisz tekst")
+spam = ham.lower()
+
+if spam.count('a') > spam.count('e'):
+    print("W tekście <<", ham, ">> znajduje się więcej liter 'a' niż liter 'e'.")
+elif spam.count('e') > spam.count('a'):
+    print("W tekście <<", ham, ">> znajduje się więcej liter 'e' niż liter 'a'.")
+elif spam.count('e') == spam.count('a'):
+    print("W tekście <<", ham, ">> znajduje się tyle samo liter 'e' jak liter 'a'.")
+else:
+    print("W tekście <<", ham, ">> nie ma żadnej litery 'a' lub 'b")
